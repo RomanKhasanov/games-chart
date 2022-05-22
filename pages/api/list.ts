39022a214573571ec9e588d59703371a4c8638c2
Roman Kh <ausstt1@gmail.com>
2022-05-22 21:22:38 +0200

    json bin

--- pages/api/list.ts
+++ pages/api/list.ts
@@ -1,5 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function list(req, res) {
-  res.status(200).json([{ name: 'Elden ring' }])
+export default async function list(req, res) { 
+  const json = await fetch(process.env.JSON_BIN_URL).then(x => x.json());
+  
+  res.status(200).json(json)
 }
