92a7e4fbb5c8924cb250878c808e137fdf5cca7a
Roman Kh <ausstt1@gmail.com>
2022-06-11 12:35:31 +0200

    deleted env, sort

--- pages/api/list.ts
+++ pages/api/list.ts
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default async function list(req, res) { 
-  const json = await fetch(process.env.JSON_BIN_URL).then(x => x.json());
+  const json = await fetch(process.env.API_URL).then(x => x.json());
   
   res.status(200).json(json)
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,11 @@
 import { useEffect, useState } from 'react'
 
+const sortFunc = function(a, b) {
+  if(a.name < b.name) { return -1; }
+  if(a.name > b.name) { return 1; }
+  return 0;
+}
+
 export default function Home() {
   const [data, setData] = useState([])
   const [isLoading, setLoading] = useState(false)
@@ -9,7 +15,7 @@ export default function Home() {
     fetch('api/list')
       .then((res) => res.json())
       .then((data) => {
-        setData(data)
+        setData(data.sort(sortFunc))
         setLoading(false)
       })
   }, [])
